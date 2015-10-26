7c0d8bb7952ab09aee42675fe95379aee0839d61
Chiel van Wonderen <wonderc001>
2015-10-26 15:24:43 +0100

    5

---
+++ SlidePuzzle/Assets/PuzzleStart.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleStart : MonoBehaviour {
+	static public int cleared;
+	public Transform full;
+	public int test;
+	public bool won;
+	public bool wonDone;
+
+
+	// Use this for initialization
+	void Start () {
+		won = false;
+		wonDone = false;
+
+	}
+
+		
+	void Update () {
+		test = cleared;
+		if(cleared == 8){
+			won = true;
+		}
+		else {
+			won = false;
+		}
+		if (won == true && wonDone == false)
+		{
+			GameObject[] gos = GameObject.FindGameObjectsWithTag("PuzzlePiece");
+			foreach(GameObject go in gos)
+				Destroy(go);
+			Instantiate(full,new Vector3(0,0,0),Quaternion.identity);
+			wonDone = true;
+		}
+
+	}
+}
